b0f294a3592392a6b588e7d7ef2c9cd8b8850380
gustavo toregrosa <gustavo.toregrosa@gmail>
2022-02-06 23:00:38 -0300

    parcial

---
+++ wordpress/wp-content/themes/wphierarchy/single.php
@@ -0,0 +1,20 @@
+<?php get_header('splash'); ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+           <?php get_template_part('template-parts/content', 'page'); ?>
+        <?php endwhile; else: ?>
+            <?php get_template_part('template-parts/content', 'none'); ?>
+        <?php endif; ?>
+        
+        <?php if(comments_open()) : ?>
+            <?php comments_template(); ?>
+        <?php endif; ?>
+
+        <p>Template single.php</p>
+    </main>
+</div>
+
+<?php get_sidebar(); ?>
+    
+<?php get_footer('splash'); ?>    
